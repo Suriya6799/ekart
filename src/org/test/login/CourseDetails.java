dff26c852b10c5e4f13b565d59bbb394611e2620
Suriya6799 <suryaarputham6153@gmail.com>
2024-11-25 18:51:50 +0530

    checking git

---
+++ src/org/test/login/CourseDetails.java
@@ -0,0 +1,24 @@
+package org.test.login;
+
+public class CourseDetails {
+	public void empId() {
+		System.out.println("EmpId=4554");
+
+	}
+	
+	public void empName() {
+		System.out.println("EmpName=Bala");
+
+	}
+	public static void main(String[] args) {
+		CourseDetails c = new CourseDetails();
+		c.empId();
+		c.empName();
+		
+		
+	
+
+	
+	}
+
+}
